c8a1cd8b899dfc52406f5c0a09ed0d658addcb61
msazmi42 <msazmi42@gmail.com>
2026-07-15 14:02:42 +0530

    Updated run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
 @echo off
-echo ======================
-echo Hello from Jenkins
-echo Date: %DATE%
-echo Time: %TIME%
-echo ======================
+echo Hello Jenkins!
+echo This is my updated file.
+pause
